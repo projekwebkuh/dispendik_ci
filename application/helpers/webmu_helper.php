9e0e6cffc7c52a004f319db9d9a4af63a0e89e4e
Fikky Alvian Firmansyah <56025920+Alvier23@users.noreply.github.com>
2020-07-03 10:42:17 +0700

    add helper

---
+++ application/helpers/webmu_helper.php
@@ -0,0 +1,10 @@
+<?php 
+    function seo_judul($s){
+        $c = array (' ');
+        $d = array ('-','/','\\',',','.','#',':',';','\'','"','[',']','{','}',')','(','|','`','~','!','@','%','$','^','&','*','=','?','+','–');
+        $s = str_replace($d, '', $s); // Hilangkan karakter yang telah disebutkan di array $d
+        $s = strtolower(str_replace($c, '-', $s)); // Ganti spasi dengan tanda - dan ubah hurufnya menjadi kecil semua
+        return $s;
+    }
+
+?>
